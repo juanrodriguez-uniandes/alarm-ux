2113a270b4e2603c2caa31a274dcbe4883139e98
Juan Pablo Rodriguez Morales <jp.rodriguezm@uniandes.edu.co>
2024-03-10 21:32:37 -0500

    Finished login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from './navbar/navbar.component';  // Asegúrate de ajustar la ruta según la ubicación de tu componente
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     NavbarComponent,
     BrowserModule,
+    AppRoutingModule,
     AppComponent,
   ],
-  bootstrap: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'signin', component: SigninComponent },
+    { path: 'home', component: HomeComponent },
+];
