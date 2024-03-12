5698fb2ac44061d4a06ab9503eb7b33fdb6f7140
Juan Pablo Rodriguez Morales <jp.rodriguezm@uniandes.edu.co>
2024-03-11 20:26:50 -0500

    Added signin view

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
-
+  constructor(private router: Router) {}
+  
+  nextStep() {
+    this.router.navigate(['/home']);
+  }
 }
